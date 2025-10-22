1b6e6942eb52e1396bd2c006c4c09d4ff47fef8d
Volker <VolkerStrack@web.de>
2025-10-22 19:12:31 +0200

    Übersetzungen & Packliste aktualisiert

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,7 +33,7 @@ export default function HomePage({ lang, setLang }) {
         </div>
 
         <p className="eyebrow pill-dark">
-          {tt("heroEyebrow", "Große Party in Tachetien in Georgien !!!")}
+          {tt("heroEyebrow", "Große Party in Shida Kartli in Georgien !!!")}
         </p>
 
         <h1 className="home-hero-title">
